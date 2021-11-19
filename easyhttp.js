fc6d212a64a816ca80082a79b818211b4c339d83
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-19 11:40:02 +0300

    callback async

---
+++ easyhttp.js
@@ -0,0 +1,25 @@
+function easyHTTP() {
+  //our http prop is an xmr instance
+  this.http = new XMLHttpRequest();
+}
+//Make an HTTP GET Request
+//should take cb to wait response fully return (async)
+easyHTTP.prototype.get = function (url, cb) {
+  //"create a get request to url async"
+  this.http.open("GET", url, true);
+  //send that request
+  this.http.send();
+  //when response arrive to bellow
+  //"this" keyword refers owner(parent,wrapper) class,object,function. wrapper of function bellow is http not easyHttp but XMLHHttpRequest! so pin easyHTTP with let self = this to "self" keyword
+  let self = this;
+  this.http.onload = function () {
+    //if response is okay
+    console.log(this);
+    if (self.http.status === 200) {
+      //since http onload async by default, methods will run async inside. cb will wait response done.
+      cb(null, self.http.responseText);
+    } else {
+      cb("Error: " + self.http.status);
+    }
+  };
+};
