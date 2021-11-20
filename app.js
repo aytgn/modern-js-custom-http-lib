d9abb3ea924e833b02203cad618f947f2133f216
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-20 12:26:09 +0300

    fetch metch

--- app.js
+++ app.js
@@ -1,52 +1,44 @@
-const http = new easyHTTP;
+//create a EasyHttp instance
+const http = new EasyHttp();
 
-// Get Posts
-// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(posts);
-//   }
-// });
-
-// Get Single Post
-// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
-
-// Create Data
-const data = {
-  title: 'Custom Post',
-  body: 'This is a custom post'
-};
-
-// Create Post
-// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
-
-// Update Post
-// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
-
-// Delete Post
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
-  if(err) {
+//MAKE GET REQUEST
+//get users and assign them to users
+const users = http
+  .get("https://jsonplaceholder.typicode.com/users") //http.get method returns promise from argument url
+  .then((users) => {
+    //when promise resolved, we get users json
+    console.log("users", users);
+  })
+  .catch((err) => {
     console.log(err);
-  } else {
-    console.log(response);
-  }
-});
+  });
+//MAKE POST
+const data = {
+  name: "John Doe",
+  username: "johnDoe",
+  email: "jdoe@gmail.com",
+};
+http
+  .post("https://jsonplaceholder.typicode.com/users", data)
+  .then((user) => {
+    console.log("user: ", user);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+//MAKE HTTP PUT REQUEST
+http
+  .put("https://jsonplaceholder.typicode.com/users/2", data)
+  .then((data) => console.log(data))
+  .catch((err) => {
+    console.log(err);
+  });
+//MAKE HTTP DELETE REQUEST
+http
+  .delete("https://jsonplaceholder.typicode.com/users/2")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ easyhttp2.js
@@ -0,0 +1,94 @@
+class EasyHttp {
+  //get(url) method will return json promise
+  get = (url) => {
+    //this promise will fetch the url
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((res) => {
+          //get the appended json response, if response is okay
+          if (res.status === 200) return res.json();
+          else {
+            throw new Error("error");
+          }
+        })
+        .then((data) => {
+          //say created promise that upon resolve bring that json
+          resolve(data);
+        })
+        .catch((err) => {
+          //say created promise that upon reject bring that error
+          reject(err);
+        });
+    });
+  };
+  post = (url, data) => {
+    //create promise, so upon resolving use it async
+    return new Promise((resolve, reject) => {
+      //fetch url with modified request options
+      fetch(url, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(data),
+      })
+        .then((res) => {
+          //if response is "created(201", return appended json
+          if (res.status === 201) return res.json();
+          else {
+            throw new Error("error");
+          }
+        })
+        .then((data) => {
+          //returned data will be new promise's resolve effect!
+          resolve(data);
+        })
+        .catch((err) => {
+          //if any error catch meanwhile, that error will be new promise's reject effect!
+          reject(data);
+        });
+    });
+  };
+  put = (url, data) => {
+    //create a promise
+    return new Promise((resolve, reject) => {
+      //create a promise to url too
+      fetch(url, {
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(data),
+      })
+        .then((res) => {
+          //upon url's response is okay, return attached json to response
+          if (res.status === 200) return res.json();
+          else {
+            throw new Error("error");
+          }
+        })
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  };
+  delete = (url) => {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "DELETE",
+        headers: { "Content-type": "application/json" },
+      })
+        .then((res) => {
+          if (res.status === 200) return res.json();
+          else {
+            throw new Error("error");
+          }
+        })
+        .then((res) => {
+          resolve("deleteeed!");
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  };
+}
